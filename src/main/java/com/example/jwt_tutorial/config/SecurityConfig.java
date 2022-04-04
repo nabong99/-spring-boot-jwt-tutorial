b4a3ae4e99ced34cfc5e5a22af1cdae74f13c5ea
nabong99 <skgusslarj@naver.com>
2022-04-05 00:42:07 +0900

    Spring Boot JWT Tutorial - JWT 코드, Security 설정 추가

--- src/main/java/com/example/jwt_tutorial/config/SecurityConfig.java
+++ src/main/java/com/example/jwt_tutorial/config/SecurityConfig.java
@@ -1,30 +1,89 @@
 package com.example.jwt_tutorial.config;
 
+import com.example.jwt_tutorial.jwt.JwtAccessDeniedHandler;
+import com.example.jwt_tutorial.jwt.JwtAuthenticationEntryPoint;
+import com.example.jwt_tutorial.jwt.JwtSecurityConfig;
+import com.example.jwt_tutorial.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
-@EnableWebSecurity //기본적인 웹 보안 활성하겠다는 annotation
-//추가적인 설정을 위해서 WebSecurityConfigurer를 inplements 하거나
-//WebSecurityConfigurerAdapter를 extends 하는 방법이 있다.
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final TokenProvider tokenProvider;
+   // private final CorsFilter corsFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+        //    CorsFilter corsFilter,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler
+    ) {
+        this.tokenProvider = tokenProvider;
+       // this.corsFilter = corsFilter;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-                .ignoring()
+    public void configure(WebSecurity web) {
+        web.ignoring()
                 .antMatchers(
                         "/h2-console/**"
-                        ,"favicon.ico"
+                        ,"/favicon.ico"
+                        ,"/error"
                 );
     }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests() //HttpServletRequest를 사용하는 요청들에 대한 접근제한 설정
-                .antMatchers("/api/hello").permitAll() //인증없이 접근 허용
-                .anyRequest().authenticated(); //나머지 요청 인증 받아야한다.
-    }
 
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf().disable()
+
+           //     .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint) //우리가 만든 클래스들로 설정해 핸들링
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // enable h2-console
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
+
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+    }
 }
