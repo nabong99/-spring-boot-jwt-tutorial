826208cd20860a873b87743e2a74d42569b034e8
nabong99 <skgusslarj@naver.com>
2022-04-06 16:20:03 +0900

    Spring Boot JWT Tutorial - DTO, Repository, 로그인

---
+++ src/main/java/com/example/jwt_tutorial/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.jwt_tutorial.repository;
+
+import com.example.jwt_tutorial.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByUsername(String username);
+}
